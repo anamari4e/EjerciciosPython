b22e6900286a2f64e7d30e365f1cc5df857771f1
Ana <amabril56@misena.edu.co>
2022-03-18 11:53:27 -0500

    ejercicio3
    Desarrollo ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,4 @@
+base=4
+altura=5
+area= base*altura/2
+print("El area de un triangulo es:", round (area,2) )
---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+capital= 10000
+interes= 3.5
+años= 7
+aumento= 
+print("El valor futuro de mi capital es:", (aumento) )
