3887bd08195e5547c013161e42b78eaa17dcd765
Ana <amabril56@misena.edu.co>
2022-03-23 06:54:54 -0500

    ejercicio 4
    desarolllo del ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
 capital= 10000
 interes= 3.5
 años= 7
-aumento= 
-print("El valor futuro de mi capital es:", (aumento) )
+valor= capital*(1+interes)**años
+print("El valor futuro de mi capital es:", (valor) )
