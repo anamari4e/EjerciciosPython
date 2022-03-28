3598ac39a6f70afb1e8128cf8af00eae88c6d9ef
Ana <amabril56@misena.edu.co>
2022-03-28 08:25:56 -0500

    cadena4
    desarrollo de cadena 4

--- cadenas4.py
+++ cadenas4.py
@@ -1,1 +1,6 @@
-suma= 5+55+555
+entrada_entero= int(input("Introduzca un valor entero:"))
+
+entrada_entero= str(entrada_entero)
+resultado= int(entrada_entero)+ int(entrada_entero*2)+ int(entrada_entero*3)
+
+print(resultado)
---
+++ cadenas5.py
@@ -0,0 +1,11 @@
+palabra= "ordenador"
+palabra_lenght=len(palabra)
+
+num_a= palabra.count("a")
+num_e= palabra.count("e")
+num_i= palabra.count("i")
+num_o= palabra.count("o")
+num_u= palabra.count("u")
+
+metric=palabra_lenght*(num_a+num_e+num_i+num_o+num_u)
+print= metric
